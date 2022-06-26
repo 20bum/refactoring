f7c6093d67ecdd0e751768651615e72d8da31a1a
20bum <20bum87@gmail.com>
2022-06-26 23:45:33 +0900

    edit chapter01

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -22,36 +22,19 @@ public class Customer {
         String result = getName() + " 고객님의 대여기록\n";
         while (rentals.hasMoreElements()) {
             double thisAmount = 0;
-            Rental each= (Rental) rentals.nextElement();
+            Rental each = (Rental) rentals.nextElement();
 
-            // 비디오 종류별 대여료 계산
-            switch (each.get_movie().get_priceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.get_daysRented() > 2)
-                        thisAmount += (each.get_daysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.get_daysRented() * 3;
-                    break;
-                case Movie.CHILDREN:
-                    thisAmount += 1.5;
-                    if (each.get_daysRented() > 3)
-                        thisAmount += (each.get_daysRented() - 3) * 1.5;
-                    break;
-            }
-
-            // 적립 포인트를 1 포인트 증가가
-            frequentRenterPoints ++;
+            // 경우에 따른 적립 포인트 지급 함수를 호출
+            frequentRenterPoints = each.getFrequentRenterPoints();
             // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
             if ((each.get_movie().get_priceCode() == Movie.NEW_RELEASE) && 
                     each.get_daysRented() > 1) frequentRenterPoints ++;
             
             // 이번에 대여하는 비디오 정보와 대여료를 출력
             result += "\t" + each.get_movie().get_title() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
+                    String.valueOf(each.getCharge()) + "\n";
             // 현재까지 누적된 총 대여료
-            totalAmount += thisAmount;
+            totalAmount += each.getCharge();
         }
         
         // 푸터 행 추가
--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -6,4 +6,35 @@ import lombok.Getter;
 public class Rental {
     private Movie _movie;
     private int _daysRented;
+
+    public double getCharge() {
+        double result = 0;
+
+        // 비디오 종류별 대여료 계산
+        switch (get_movie().get_priceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (get_daysRented() > 2)
+                    result += (get_daysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += get_daysRented() * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (get_daysRented() > 3)
+                    result += (get_daysRented() - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
+    // 최신물을 이틀이상 대여하면 2포인트 지급하고 그 외엔 1포인트 지급하는 코드를
+    // 빼내 getFrequentRenterPoints 메서드로 만들고 이 Rental 클래스로 옮겼다.
+    public int getFrequentRenterPoints() {
+        if ((get_movie().get_priceCode() == Movie.NEW_RELEASE) && get_daysRented() > 1)
+            return 2;
+        else
+            return 1;
+    }
 }
